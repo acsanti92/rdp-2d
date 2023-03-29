8138c176db864cc0057a390238a74bc86779683e
acsanti92 <armando.santiago@pluto.tv>
2023-03-29 16:25:41 -0700

    Running and Jumping Animations

--- ready-pluto-one-2d/Assets/Scripts/PlayerController.cs
+++ ready-pluto-one-2d/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,14 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     // Jump force of the player
     public float jumpForce;
+    // Reference to the Transform component called "groundPoint"
+    public Transform groundPoint;
+    // We are going to use this to check if the player is on the ground
+    private bool isOnGround;
+    // A way to check what is considered ground
+    public LayerMask whatIsGround;
+    // Reference to the Animator component called "theAnim"
+    public Animator theAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +31,27 @@ public class PlayerController : MonoBehaviour
         // Move the player left and right
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
 
+        // Handle Direction Change 
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        // Checking if the player is on the ground
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
         }
+
+        // Set the animation
+        theAnim.SetBool("isOnGround", isOnGround);
+        theAnim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
     }
 }
