d91281d37714dce95ec8554df6a43b86552e7d09
acsanti92 <armando.santiago@pluto.tv>
2023-03-30 12:20:11 -0700

    Player After Image Effects

--- ready-pluto-one-2d/Assets/Scripts/PlayerController.cs
+++ ready-pluto-one-2d/Assets/Scripts/PlayerController.cs
@@ -28,6 +28,14 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed, dashTime;
     // Dash Counter
     private float dashCounter;
+    // Render the after image
+    public SpriteRenderer theSR, afterImage;
+    // Value to control the after image
+    public float afterImageLifetime, timeBetweenAfterImages;
+    // Keeping track of how long we wait between after images
+    private float afterImageCounter;
+    // After Image Color
+    public Color afterImageColor;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +49,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Fire2"))
         {
             dashCounter = dashTime;
+            ShowAfterImage();
         }
 
         if (dashCounter > 0)
@@ -48,6 +57,12 @@ public class PlayerController : MonoBehaviour
             dashCounter -= Time.deltaTime;
 
             theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter <= 0)
+            {
+                ShowAfterImage();
+            }
         }
         else
         {
@@ -97,5 +112,18 @@ public class PlayerController : MonoBehaviour
         // Set the animation
         theAnim.SetBool("isOnGround", isOnGround);
         theAnim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+    }
+
+    //
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+
+        Destroy(image.gameObject, afterImageLifetime);
+
+        afterImageCounter = timeBetweenAfterImages;
     }
 }
